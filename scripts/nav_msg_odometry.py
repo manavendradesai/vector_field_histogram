8aec5136351ff730142567f9359595794a85ec2d
AmbyrElan <AmbyrElan@wayne.edu>
2023-02-11 14:39:20 -0500

    constructor for nav_msgs script

--- scripts/nav_msg_odometry.py
+++ scripts/nav_msg_odometry.py
@@ -2,9 +2,32 @@
 import rospy
 
 from nav_msgs.msg import Odometry
+from nav_msgs.msg import OccupancyGrid
 from std_msgs.msg import String
+# include in manifest.xml file ? <depend package="nav_msgs"/> ?
 
-odom1_pose = (float, float, float)
+class OdomData:
+    
+    def __init__(self):
+
+        #Define subscriber to odom
+        self.odom_sub = rospy.Subscriber("/odom",Odometry,self.robot_pose)
+
+        #Define publisher to odom
+        self.odom_pub = rospy.Publisher("/odom2", String, queue_size=10)
+
+    #Callback function
+    def robot_pose(self, pose_msg):
+
+        #Robot position
+        self.x = pose_msg.pose.pose.position.x
+        self.y = pose_msg.pose.pose.position.y
+        self.z = pose_msg.pose.pose.orientation.z
+
+        print(self.x, self.y, self.z)
+
+
+"""
 
 def odometryCb(msg):
     global odom1_pose
@@ -24,16 +47,15 @@ def publisher():
         rospy.loginfo(data)
         pub.publish(data)
         rate.sleep()
+"""
 
 if __name__ == "__main__":
-    rospy.init_node('odometry', anonymous=True) #make node 
-    odom_sub = rospy.Subscriber('/odom',Odometry,odometryCb)
+    rospy.init_node('nav_msg_odometry', anonymous=True) #make node 
+
     try:
-        publisher()
+        OdomData()
+        rospy.spin()
     except rospy.ROSInterruptException:
     	pass
     	
-    rospy.spin()
     
-print(odom1_pose)
-
