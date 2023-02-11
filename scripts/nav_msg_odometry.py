f4cb3358e029f47104e55c9ef42d26d3a61a10a4
AmbyrElan <AmbyrElan@wayne.edu>
2023-02-11 15:02:40 -0500

    include occupancy grid data

--- scripts/nav_msg_odometry.py
+++ scripts/nav_msg_odometry.py
@@ -4,7 +4,6 @@ import rospy
 from nav_msgs.msg import Odometry
 from nav_msgs.msg import OccupancyGrid
 from std_msgs.msg import String
-# include in manifest.xml file ? <depend package="nav_msgs"/> ?
 
 class OdomData:
     
@@ -16,6 +15,13 @@ class OdomData:
         #Define publisher to odom
         self.odom_pub = rospy.Publisher("/odom2", String, queue_size=10)
 
+        #Define subscriber to occupancy grid
+        self.occupancy_sub = rospy.Subscriber("/move_base/local_costmap/costmap", OccupancyGrid, self.robot_pose)
+
+        #Define publisher to occupancy grid
+        self.occupancy_pub = rospy.Publisher("/occupancy_data", String, queue_size=10)
+
+
     #Callback function
     def robot_pose(self, pose_msg):
 
@@ -26,31 +32,16 @@ class OdomData:
 
         print(self.x, self.y, self.z)
 
+    #Callback function to
+    def get_local_costmap(self, costmap_msg):
 
-"""
-
-def odometryCb(msg):
-    global odom1_pose
-    odom1_pose = (msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.orientation.z)
-    print(msg.pose.pose)
-    
-def publisher():
-    pub = rospy.Publisher('/odom2',String, queue_size=10)
-    
-   #rospy.init_node('odometry2', anonymous=True) #make node
-    
-    rate = rospy.Rate(1) #define rate/sec 
-    
-    while not rospy.is_shutdown():
-        data = "This is odom 2"
-        
-        rospy.loginfo(data)
-        pub.publish(data)
-        rate.sleep()
-"""
+        #Receive local costmap data as 1D array
+        self.local_costmap = costmap_msg.data
 
 if __name__ == "__main__":
-    rospy.init_node('nav_msg_odometry', anonymous=True) #make node 
+
+    #make node 
+    rospy.init_node('nav_msg_odometry', anonymous=True)
 
     try:
         OdomData()
